e2573ec7afabcc8f5e02ce1c1c01ec43792ecf4a
Fidel Caicedo <869@holbertonschool.com>
2020-01-03 16:09:02 -0500

    uploading corrected version

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,15 +3,10 @@
 apt-get update -y
 apt-get install -y nginx
 DIR="/data/web_static/shared/"
-if [ ! -d "${DIR}" ];
-then
-	mkdir ${DIR}
-fi
+mkdir -p ${DIR}
 DIR="/data/web_static/releases/test/"
-if [ ! -d "${DIR}" ];
-then
-	mkdir ${DIR}
-fi
+mkdir -p ${DIR}
+touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
@@ -21,8 +16,8 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "46i location ^/hbnb_static {" /etc/nginx/sites-available/default
-sed -i "47i   alias /data/web_static/current/;" /etc/nginx/sites-available/default
-sed -i "48i   autoindex off;" /etc/nginx/sites-available/default
-sed -i "49i }" /etc/nginx/sites-available/default
-
+sed -i "46i location /hbnb_static {" /etc/nginx/sites-enabled/default
+sed -i "47i   alias /data/web_static/current/;" /etc/nginx/sites-enabled/default
+sed -i "48i   autoindex off;" /etc/nginx/sites-enabled/default
+sed -i "49i }" /etc/nginx/sites-enabled/default
+service nginx restart
