edc4238d5b4ce1f755828a6f1bdd14db690a6418
Fidel Caicedo <869@holbertonschool.com>
2020-01-03 21:47:49 -0500

    uploading completeded second point

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Leave this place Nerevar, only death awaits """
-from fabric.api import local, put, run
+from fabric.api import local, put, run, env
 from datetime import datetime as dt
 from os import path
 
@@ -20,20 +20,25 @@ def do_pack():
         return tgz
 
 
+env.host = ['34.74.97.231', '35.227.41.101']
+
+
 def do_deploy(archive_path):
     """ distributes an archive to the web servers """
-    if archive_path is None:
+    if not path.exists(archive_path):
         return False
-    noexten = path.splitext(archive_path)[0]
+    splitted = archive_path.split("/")
+    noexten = path.splitext(splitted[1])[0]
+
     try:
-        put(archive_path, "tmp")
+        put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(noexten))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_path, noexten))
-        run("rm /tmp/{}".format(archive_path))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(splitted[1], noexten))
+        run("rm /tmp/{}".format(splitted[1]))
         run("mv /data/web_static/releases/{}/web_static/*  /data/web_static/releases/{}/".format(noexten, noexten))
         run("rm -rf /data/web_static/releases/{}/web_static".format(noexten))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(noexten))
         return True
-    except:
+    except Exception:
         return False
