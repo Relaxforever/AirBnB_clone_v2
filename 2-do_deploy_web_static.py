44311081e351280508e47943d81539805adcc44a
Fidel Caicedo <869@holbertonschool.com>
2020-01-03 21:54:14 -0500

    uploading completeded second point with pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,13 +32,16 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(noexten))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(splitted[1], noexten))
+        run("mkdir -p /data/web_static/releases/{}".format(noexten))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(splitted[1], noexten))
         run("rm /tmp/{}".format(splitted[1]))
-        run("mv /data/web_static/releases/{}/web_static/*  /data/web_static/releases/{}/".format(noexten, noexten))
+        run("mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/".format(noexten, noexten))
         run("rm -rf /data/web_static/releases/{}/web_static".format(noexten))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(noexten))
+        run("ln -s /data/web_static/releases/{}/\
+         /data/web_static/current".format(noexten))
         return True
     except Exception:
         return False
