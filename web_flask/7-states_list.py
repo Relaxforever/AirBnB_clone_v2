009599cb47cc468cf48c566c53fb7b51f073cc2e
Fidel Caicedo <869@holbertonschool.com>
2020-01-21 18:28:01 -0500

    UPloading eight point

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def finish_session(NaN):
+    """ Tears down the session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Prints hello When someone enters / """
+    return render_template('7-states_list.html',
+                           dict_states=storage.all(State))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
